fc2f52c41355e3cacbb7fc907df2373d05a5ba18
SangBeom-Lee <bumeeya2@naver.com>
2021-06-19 15:20:11 +0900

    "Movie GraphQl"

---
+++ graphql/resolvers.js
@@ -0,0 +1,9 @@
+import { getMovies, getById } from "./db";
+
+const resolvers                                 = {
+    Query                                       : {
+        movies                                  : (_, {limit, rating}) => getMovies(limit, rating)
+    }
+};
+
+export default resolvers;
